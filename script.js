300603179955a8a79e45c4635933bf88e502b387
pavithra29282 <thipshikapavi@gmail.com>
2026-07-14 13:40:38 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const name = document.getElementById("name").value;
+const animal = document.getElementById("animal").value;
+const number = document.getElementById("number").value;
+const adjective = document.getElementById("adjective").value;
+const theme = document.getElementById("theme").value;
+const story = document.getElementById("story");
+story.innerHTML = 
