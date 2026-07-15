a962194c126530ad4e419aff9a7536e6014f8fbf
moganesan <developer.moganesan@gmail.com>
2026-07-15 15:25:15 +0530

    form fixed

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ function submitForm() {
     var story = document.getElementById("story");
 
     const storyText =
-        `Once upon a time, ${name} discovered a mysterious ${theme} adventure. ` +
-        `While exploring, ${name} encountered ${number} ${animal}${number > 1 ? "s" : ""}. ` +
-        `Although feeling very ${adjective}, ${name} decided to keep going. ` +
+        `Once upon a time, ${fname} discovered a mysterious ${theme} adventure. ` +
+        `While exploring, ${fname} encountered ${number} ${animal}${number > 1 ? "s" : ""}. ` +
+        `Although feeling very ${adjective}, ${fname} decided to keep going. ` +
         `In the end, the adventure became an unforgettable story with a happy ending.`;
 
     story.innerHTML = storyText;
